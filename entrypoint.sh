8c4dc9b0aa1047f15f8e7c899dd27fa46af6e30a
Stanley Ayo <107850984+stanmart1@users.noreply.github.com>
2025-10-05 19:42:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,13 @@ if [ ! -e /dev/console ]; then
     ln -sf /dev/null /dev/console
 fi
 
+# Ensure /storage exists and is writable
+if [ ! -d /storage ]; then
+    mkdir -p /storage
+fi
+
+echo "Fixing permissions for /storage..."
+chmod -R 777 /storage || true
+
 # Hand over control to the original entrypoint
 exec /sbin/docker-entrypoint "$@"
